ee93dae37d1c497ae90959cf667cba98079dba05
guillaume gandon <guillaume@gandon.name>
2024-04-30 20:48:17 +0200

    skilltreefin

---
+++ Assets/Script/Character/Player/Skill System/UI/UITalentButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+namespace Script.Player.Skill_System.UI
+{
+    public class UITalentButton
+    {
+        private Button _button;
+        private ScriptableSkill _skill;
+        private bool _isUnlocked = false;
+        public static UnityAction<ScriptableSkill> OnSkillButtonClicked;
+        public UITalentButton(Button assignedButton, ScriptableSkill assignedSkill)
+        {
+            _button = assignedButton;
+            _button.clicked += OnClick;
+            _skill = assignedSkill;
+            if (assignedSkill.Icon) _button.style.backgroundImage = new StyleBackground(assignedSkill.Icon);
+        }
+
+        private void OnClick()
+        {
+            OnSkillButtonClicked?.Invoke(_skill);
+        }
+    }
+}
